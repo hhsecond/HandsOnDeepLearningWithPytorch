5e8f18a26fc8e25834499bbc3aef9efab3c738d7
sherin thomas <sherinct@live.com>
2017-12-25 15:09:43 +0530

    custom maxpool

--- 4.ComputerVision/simpleCNN.py
+++ 4.ComputerVision/simpleCNN.py
@@ -21,12 +21,12 @@ transform = transforms.Compose(
 def get_data():
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                             download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=100,
                                               shuffle=True, num_workers=2)
 
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                            download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+    testloader = torch.utils.data.DataLoader(testset, batch_size=100,
                                              shuffle=False, num_workers=2)
 
     classes = ('plane', 'car', 'bird', 'cat',
--- 4.ComputerVision/simpleCNNModel.py
+++ 4.ComputerVision/simpleCNNModel.py
@@ -37,8 +37,40 @@ class Conv(nn.Module):
         for nf, f in enumerate(self.filters):
             for h in range(new_height):
                 for w in range(new_width):
-                    val = padded_input[:, :, h:h + self.kernel_size, w:w + self.kernel_size] * f
-                    out[:, nf, h, w] = val.sum()
+                    val = padded_input[:, :, h:h + self.kernel_size, w:w + self.kernel_size]
+                    out[:, nf, h, w] = val.contiguous().view(batch_size, -1) @ f.view(-1)
+        return out
+
+
+class MaxPool(nn.Module):
+    """
+    Custom max pool layer
+    """
+
+    def __init__(self, kernel_size):
+        super().__init__()
+        self.kernel_size = kernel_size
+
+    def forward(self, x):
+        # expected size = batch x depth x height x width
+        if len(x.size()) != 4:
+            raise Exception('Batch should be 4 dimensional')
+        batch_size = x.size(0)
+        depth = x.size(1)
+        height = x.size(2)
+        width = x.size(3)
+        new_height = int(((height - self.kernel_size) / self.kernel_size) + 1)
+        new_width = int(((width - self.kernel_size) / self.kernel_size) + 1)
+        if height != width:
+            raise Exception('Only processing square Image')
+        if height % self.kernel_size != 0:
+            raise Exception('Keranl cannot be moved completely, change Kernal size')
+        out = Variable(torch.zeros(batch_size, depth, new_height, new_width))
+        for h in range(new_height):
+            for w in range(new_width):
+                for d in range(depth):
+                    val = x[:, d, h:h + self.kernel_size, w:w + self.kernel_size]
+                    out[:, d, h, w] = val.max(2)[0].max(1)[0]
         return out
 
 
