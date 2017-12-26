2f45dd3b2d5f691eebb229fdb6109ac069fb8844
sherin thomas <sherinct@live.com>
2017-12-26 09:14:52 +0530

    training and testing loop

--- 4.ComputerVision/simpleCNN.py
+++ 4.ComputerVision/simpleCNN.py
@@ -1,8 +1,10 @@
 import torch
+from torch import nn
+from torch import optim
 from torch.autograd import Variable
 import torchvision
 import torchvision.transforms as transforms
-from simpleCNNModel import Conv
+from simpleCNNModel import SimpleCNNModel
 
 
 ###############
@@ -13,28 +15,55 @@ from simpleCNNModel import Conv
 # pool > down sampling (But trends are prefering strides over pooling), max, min, average
 ###############
 
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
 
 def get_data():
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=100,
-                                              shuffle=True, num_workers=2)
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    trainset = torchvision.datasets.CIFAR10(
+        root='./data', train=True, download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(
+        trainset, batch_size=100, shuffle=True, num_workers=2)
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                           download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=100,
-                                             shuffle=False, num_workers=2)
-
-    classes = ('plane', 'car', 'bird', 'cat',
-               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    testset = torchvision.datasets.CIFAR10(
+        root='./data', train=False, download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(
+        testset, batch_size=100, shuffle=False, num_workers=2)
+    return trainloader, testloader
 
 
-myconv = Conv(2, 4, 1)
-actualconv = torch.nn.Conv2d(2, 4, 1)
-x = Variable(torch.rand(1, 2, 3, 3))
-print(myconv(x))
-print(actualconv(x))
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+net = SimpleCNNModel()
+loss_fn = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+trainloader, testloader = get_data()
+
+print('Training Started..')
+for epoch in range(2):
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+        optimizer.zero_grad()
+        outputs = net(Variable(inputs))
+        loss = loss_fn(outputs, Variable(labels))
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.data[0]
+        check_interval = 2
+        if i % check_interval == check_interval - 1:
+            print('Iteration: {}, loss: {:.6f}'.format(
+                (epoch + 1) * (i + 1), running_loss / check_interval))
+            running_loss = 0.0
+
+print('Testing Started..')
+correct = 0
+total = 0
+for data in testloader:
+    images, labels = data
+    outputs = net(Variable(images))
+    index, value = torch.max(outputs.data, 1)
+    total += labels.size(0)
+    correct += (value == labels).sum()
+
+print('Accuracy: {:.5f}'.format(100 * correct / total))
