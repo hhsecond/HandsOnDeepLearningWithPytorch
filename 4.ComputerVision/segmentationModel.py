81aef9ad0ee7423d891935713d1fe65b4dd7a85d
sherin thomas <sherinct@live.com>
2017-12-30 17:01:40 +0530

    semantic segmentation init

---
+++ 4.ComputerVision/segmentationModel.py
@@ -0,0 +1,12 @@
+import torch
+from torch import nn
+
+
+class SegmentationModel(nn.Module):
+    """
+    LinkNet for Semantic segmentation. Inspired heavily by
+    https://github.com/meetshah1995/pytorch-semseg
+    """
+
+    def __init__(self):
+        pass
