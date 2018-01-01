84096f45011c464f48c8ddd119c6e499066c0e6f
sherin thomas <sherinct@live.com>
2018-01-01 23:21:29 +0530

    loader

--- 4.ComputerVision/SemSeg/segmentation.py
+++ 4.ComputerVision/SemSeg/segmentation.py
@@ -1,4 +1,13 @@
-from loader import CamvidLoader
+from torch.utils import data
 
+from dataset import CamvidDataSet
+from segmentationModel import SegmentationModel
+
+s = SegmentationModel()
 # Training
-Loader = CamvidLoader('train')
+path = '/home/hhsecond/mypro/ThePyTorchBook/ThePyTorchBookDataSet/camvid'
+dataset = CamvidDataSet('train', path)
+loader = data.DataLoader(dataset, batch_size=1, num_workers=1, shuffle=True)
+
+for batch in loader:
+    print(len(batch))
