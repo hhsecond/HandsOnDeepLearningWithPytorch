fbc67e712e2977d987c1bd0523c1536330a1f116
sherin thomas <sherinct@live.com>
2018-01-03 23:57:11 +0530

    decoder incomplete

--- 4.ComputerVision/SemSeg/segmentationModel.py
+++ 4.ComputerVision/SemSeg/segmentationModel.py
@@ -10,9 +10,8 @@ class SegmentationModel(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.initial_block = nn.Sequential(
-            ConvBlock(inp=3, out=64, kernal=7, stride=2, pad=3, bias=False, act=True),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1))
+        self.initial_conv = ConvBlock(inp=3, out=64, kernal=7, stride=2, pad=3, bias=False, act=True)
+        self.initial_maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
         self.encoder1 = EncoderBlock(inp=64, out=64, downsample=False)
         self.encoder2 = EncoderBlock(inp=64, out=128, downsample=True)
@@ -24,18 +23,41 @@ class SegmentationModel(nn.Module):
         self.decoder2 = DecoderBlock(inp=128, out=64)
         self.decoder1 = DecoderBlock(inp=64, out=64)
 
-        self.final_block = nn.Sequential(
-            DeconvBlock(inp=64, out=32, kernal=3, stride=2, pad=1),
-            ConvBlock(inp=32, out=32, kernal=3, stride=1, pad=1, bias=True, act=True),
-            DeconvBlock(inp=32, out=1, kernal=2, stride=2, pad=0))
+        self.final_deconv1 = DeconvBlock(inp=64, out=32, kernal=3, stride=2, pad=1)
+        self.final_conv = ConvBlock(inp=32, out=32, kernal=3, stride=1, pad=1, bias=True, act=True)
+        self.final_deconv2 = DeconvBlock(inp=32, out=1, kernal=2, stride=2, pad=0)
 
     def forward(self, x):
-        start = self.initial_block(x)
-        e1 = self.encoder1(start)
+        start_conv = self.initial_conv(x)
+        start_maxpool = self.initial_maxpool(start_conv)
+        print(start_maxpool.size())
+        print('################3')
+        e1 = self.encoder1(start_maxpool)
+        print(e1.size())
         e2 = self.encoder2(e1)
+        print(e2.size())
         e3 = self.encoder3(e2)
+        print(e3.size())
         e4 = self.encoder4(e3)
-        return e4
+        print(e4.size())
+        print('################3')
+
+        d4 = self.decoder4(e4, output_size=e3.size()) + e3
+        print('<<<<<<<<<<<<<<<<<<')
+        print(d4.size())
+        d3 = self.decoder3(d4, output_size=e2.size()) + e2
+        print(d3.size())
+        d2 = self.decoder2(d3, output_size=e1.size()) + e1
+        print(d2.size())
+        d1 = self.decoder1(d2, output_size=start_maxpool.size())
+        print(d1.size())
+        print('>>>>>>>>>>>>>>>>>')
+
+        final_deconv1 = self.final_deconv1(d1, output_size=start_conv.size())
+        final_conv = self.final_conv(final_deconv1)
+        final_deconv2 = self.final_deconv2(final_conv, output_size=x.size())
+
+        return final_deconv2
 
 
 class EncoderBlock(nn.Module):
@@ -79,9 +101,18 @@ class DecoderBlock(nn.Module):
 
     def __init__(self, inp, out):
         super().__init__()
+        self.conv1 = ConvBlock(
+            inp=inp, out=inp // 4, kernal=1, stride=1, pad=0, bias=True, act=True)
+        self.deconv = DeconvBlock(
+            inp=inp // 4, out=inp // 4, kernal=3, stride=2, pad=1)
+        self.conv2 = ConvBlock(
+            inp=inp // 4, out=out, kernal=1, stride=1, pad=0, bias=True, act=True)
 
-    def forward(self, x):
-        pass
+    def forward(self, x, output_size):
+        conv1 = self.conv1(x)
+        deconv = self.deconv(conv1, output_size=output_size)
+        conv2 = self.conv2(deconv)
+        return conv2
 
 
 class ConvBlock(nn.Module):
@@ -108,10 +139,12 @@ class DeconvBlock(nn.Module):
 
     def __init__(self, inp, out, kernal, stride, pad):
         super().__init__()
-        self.deconv_block = nn.Sequential(
-            nn.ConvTranspose2d(inp, out, kernal, stride, pad),
-            nn.BatchNorm2d(out),
-            nn.ReLU())
+        self.conv_transpose = nn.ConvTranspose2d(inp, out, kernal, stride, pad)
+        self.batchnorm = nn.BatchNorm2d(out)
+        self.relu = nn.ReLU()
 
-    def forward(self, x):
-        return self.deconv_block(x)
+    def forward(self, x, output_size):
+        print(self, x.size(), output_size)
+        convt_out = self.conv_transpose(x, output_size=output_size)
+        batchnormout = self.batchnorm(convt_out)
+        return self.relu(batchnormout)
