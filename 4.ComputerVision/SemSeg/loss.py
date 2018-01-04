6e4e4de977841a635252172ff1adbf6e7d7b43ea
sherin thomas <sherinct@live.com>
2018-01-04 22:46:29 +0530

    crossentropy2d

---
+++ 4.ComputerVision/SemSeg/loss.py
@@ -0,0 +1,13 @@
+from torch import nn
+import torch.nn.functional as F
+
+
+class CrossEntropyLoss2d(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+        self.loss = nn.NLLLoss2d()
+
+    def forward(self, outputs, targets):
+        return self.loss(F.log_softmax(outputs), targets)
