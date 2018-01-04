2d9a232df71d6daa5c5ceec6c5d56ca551dbb2ed
sherin thomas <sherinct@live.com>
2018-01-04 11:22:50 +0530

    decoder

--- 4.ComputerVision/SemSeg/segmentationModel.py
+++ 4.ComputerVision/SemSeg/segmentationModel.py
@@ -6,17 +6,20 @@ class SegmentationModel(nn.Module):
     LinkNet for Semantic segmentation. Inspired heavily by
     https://github.com/meetshah1995/pytorch-semseg
     # TODO -> pad = kernal // 2
+    # TODO -> change the var names
+    # find size > a = lambda n, f, p, s: (((n + (2 * p)) - f) / s) + 1
+    # Cannot have resnet18 architecture because it doesn't do downsampling on first layer
     """
 
     def __init__(self):
         super().__init__()
-        self.initial_conv = ConvBlock(inp=3, out=64, kernal=7, stride=2, pad=3, bias=False, act=True)
-        self.initial_maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.init_conv = ConvBlock(inp=3, out=64, kernal=7, stride=2, pad=3, bias=False, act=True)
+        self.init_maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        self.encoder1 = EncoderBlock(inp=64, out=64, downsample=False)
-        self.encoder2 = EncoderBlock(inp=64, out=128, downsample=True)
-        self.encoder3 = EncoderBlock(inp=128, out=256, downsample=True)
-        self.encoder4 = EncoderBlock(inp=256, out=512, downsample=True)
+        self.encoder1 = EncoderBlock(inp=64, out=64)
+        self.encoder2 = EncoderBlock(inp=64, out=128)
+        self.encoder3 = EncoderBlock(inp=128, out=256)
+        self.encoder4 = EncoderBlock(inp=256, out=512)
 
         self.decoder4 = DecoderBlock(inp=512, out=256)
         self.decoder3 = DecoderBlock(inp=256, out=128)
@@ -28,34 +31,21 @@ class SegmentationModel(nn.Module):
         self.final_deconv2 = DeconvBlock(inp=32, out=1, kernal=2, stride=2, pad=0)
 
     def forward(self, x):
-        start_conv = self.initial_conv(x)
-        start_maxpool = self.initial_maxpool(start_conv)
-        print(start_maxpool.size())
-        print('################3')
-        e1 = self.encoder1(start_maxpool)
-        print(e1.size())
+        init_conv = self.init_conv(x)
+        init_maxpool = self.init_maxpool(init_conv)
+        e1 = self.encoder1(init_maxpool)
         e2 = self.encoder2(e1)
-        print(e2.size())
         e3 = self.encoder3(e2)
-        print(e3.size())
         e4 = self.encoder4(e3)
-        print(e4.size())
-        print('################3')
 
-        d4 = self.decoder4(e4, output_size=e3.size()) + e3
-        print('<<<<<<<<<<<<<<<<<<')
-        print(d4.size())
-        d3 = self.decoder3(d4, output_size=e2.size()) + e2
-        print(d3.size())
-        d2 = self.decoder2(d3, output_size=e1.size()) + e1
-        print(d2.size())
-        d1 = self.decoder1(d2, output_size=start_maxpool.size())
-        print(d1.size())
-        print('>>>>>>>>>>>>>>>>>')
+        d4 = self.decoder4(e4, e3.size()) + e3
+        d3 = self.decoder3(d4, e2.size()) + e2
+        d2 = self.decoder2(d3, e1.size()) + e1
+        d1 = self.decoder1(d2, init_maxpool.size())
 
-        final_deconv1 = self.final_deconv1(d1, output_size=start_conv.size())
+        final_deconv1 = self.final_deconv1(d1, init_conv.size())
         final_conv = self.final_conv(final_deconv1)
-        final_deconv2 = self.final_deconv2(final_conv, output_size=x.size())
+        final_deconv2 = self.final_deconv2(final_conv, x.size())
 
         return final_deconv2
 
@@ -63,7 +53,7 @@ class SegmentationModel(nn.Module):
 class EncoderBlock(nn.Module):
     """ Residucal Block in linknet that does Encoding - layers in ResNet18 """
 
-    def __init__(self, inp, out, downsample):
+    def __init__(self, inp, out):
         """
         Resnet18 has first layer without downsampling.
         The parameter ``downsampling`` decides that
@@ -71,28 +61,19 @@ class EncoderBlock(nn.Module):
         # in downsample
         """
         super().__init__()
-        self.downsample = downsample
-        if self.downsample:
-            self.block1 = nn.Sequential(
-                ConvBlock(inp=inp, out=out, kernal=3, stride=2, pad=1, bias=False, act=True),
-                ConvBlock(inp=out, out=out, kernal=3, stride=1, pad=1, bias=False, act=False))
-            self.residue = ConvBlock(
-                inp=inp, out=out, kernal=1, stride=2, pad=0, bias=False, act=False)
-        else:
-            self.block1 = nn.Sequential(
-                ConvBlock(inp=inp, out=out, kernal=3, stride=1, pad=1, bias=False, act=True),
-                ConvBlock(inp=out, out=out, kernal=3, stride=1, pad=1, bias=False, act=False))
+        self.block1 = nn.Sequential(
+            ConvBlock(inp=inp, out=out, kernal=3, stride=2, pad=1, bias=False, act=True),
+            ConvBlock(inp=out, out=out, kernal=3, stride=1, pad=1, bias=False, act=False))
         self.block2 = nn.Sequential(
             ConvBlock(inp=out, out=out, kernal=3, stride=1, pad=1, bias=False, act=True),
             ConvBlock(inp=out, out=out, kernal=3, stride=1, pad=1, bias=False, act=False))
+        self.residue = ConvBlock(
+            inp=inp, out=out, kernal=1, stride=2, pad=0, bias=False, act=False)
 
     def forward(self, x):
         out1 = self.block1(x)
-        if self.downsample:
-            residue = self.residue(x)
-            out2 = self.block2(out1 + residue)
-        else:
-            out2 = self.block2(out1 + x)
+        residue = self.residue(x)
+        out2 = self.block2(out1 + residue)
         return out2 + out1
 
 
@@ -144,7 +125,6 @@ class DeconvBlock(nn.Module):
         self.relu = nn.ReLU()
 
     def forward(self, x, output_size):
-        print(self, x.size(), output_size)
         convt_out = self.conv_transpose(x, output_size=output_size)
         batchnormout = self.batchnorm(convt_out)
         return self.relu(batchnormout)
