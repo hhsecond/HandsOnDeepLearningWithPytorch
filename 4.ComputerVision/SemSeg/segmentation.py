fef3615d345ca8261bfcd6ba909881736788795e
sherin thomas <sherinct@live.com>
2018-01-05 17:24:28 +0530

    training

--- 4.ComputerVision/SemSeg/segmentation.py
+++ 4.ComputerVision/SemSeg/segmentation.py
@@ -1,8 +1,9 @@
 import torch
 from torch.utils import data
 from torch.autograd import Variable
+from torch import nn
+import torch.nn.functional as F
 
-from loss import CrossEntropyLoss2d
 from dataset import CamvidDataSet
 from segmentationModel import SegmentationModel
 
@@ -14,17 +15,16 @@ net = SegmentationModel()
 # Training
 path = '/home/hhsecond/mypro/ThePyTorchBook/ThePyTorchBookDataSet/camvid'
 dataset = CamvidDataSet('train', path)
-loader = data.DataLoader(dataset, batch_size=1, num_workers=1, shuffle=True)
+loader = data.DataLoader(dataset, batch_size=8, num_workers=4, shuffle=True)
 optimizer = torch.optim.Adam(net.parameters())
-loss_fn = CrossEntropyLoss2d()
+loss_fn = nn.NLLLoss2d()
 
 
-for in_batch, out_batch in loader:
+for in_batch, target_batch in loader:
+    optimizer.zero_grad()
     out = net(Variable(in_batch))
-    print(out.size(), out_batch.size())
-    print(type(out_batch.max()))
-    if out_batch.max() == 0:
-        print('not doing it >>>>>>>>>>>>')
-    else:
-        print(out_batch.max())
-        loss = loss_fn(out, Variable(out_batch))
+    loss = loss_fn(F.log_softmax(out, 1), Variable(target_batch))
+    loss.backward()
+    optimizer.step()
+    # TODO - make the visualization for this soon
+    print('Loss: {:.5f}'.format(loss.data[0]))
