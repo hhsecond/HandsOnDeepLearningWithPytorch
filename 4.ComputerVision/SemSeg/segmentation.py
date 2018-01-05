ab117a3d2e4260705592f387b98550e4a16fa154
sherin thomas <sherinct@live.com>
2018-01-05 17:57:49 +0530

    200 epochs

--- 4.ComputerVision/SemSeg/segmentation.py
+++ 4.ComputerVision/SemSeg/segmentation.py
@@ -18,19 +18,21 @@ else:
     net = SegmentationModel()
 # Training
 path = '/home/hhsecond/mypro/ThePyTorchBook/ThePyTorchBookDataSet/camvid'
+epochs = 200
 dataset = CamvidDataSet('train', path)
-loader = data.DataLoader(dataset, batch_size=1, num_workers=1, shuffle=True)
+loader = data.DataLoader(dataset, batch_size=16, num_workers=4, shuffle=True)
 optimizer = torch.optim.Adam(net.parameters())
 loss_fn = nn.NLLLoss2d()
 
 
-for in_batch, target_batch in loader:
-    if is_cuda:
-        in_batch, target_batch = in_batch.cuda(), target_batch.cuda()
-    optimizer.zero_grad()
-    out = net(Variable(in_batch))
-    loss = loss_fn(F.log_softmax(out, 1), Variable(target_batch))
-    loss.backward()
-    optimizer.step()
-    # TODO - make the visualization for this soon
-    print('Loss: {:.5f}'.format(loss.data[0]))
+for epoch in epochs:
+    for in_batch, target_batch in loader:
+        if is_cuda:
+            in_batch, target_batch = in_batch.cuda(), target_batch.cuda()
+        optimizer.zero_grad()
+        out = net(Variable(in_batch))
+        loss = loss_fn(F.log_softmax(out, 1), Variable(target_batch))
+        loss.backward()
+        optimizer.step()
+        # TODO - make the visualization for this soon
+        print('Loss: {:.5f}, Epochs: {:3d}'.format(loss.data[0], epoch))
