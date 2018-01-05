6f8b565fc2f506715e8f8ecc6ec9ec6a785b022d
sherin thomas <sherinct@live.com>
2018-01-05 19:29:33 +0530

    clean up

--- 4.ComputerVision/SemSeg/segmentation.py
+++ 4.ComputerVision/SemSeg/segmentation.py
@@ -21,7 +21,7 @@ net.train()
 # Training
 path = '/home/hhsecond/mypro/ThePyTorchBook/ThePyTorchBookDataSet/camvid'
 epochs = 64
-bsize = 2
+bsize = 8
 dataset = CamvidDataSet('train', path)
 loader = data.DataLoader(dataset, batch_size=bsize, num_workers=4, shuffle=True)
 optimizer = torch.optim.Adam(net.parameters())
@@ -41,7 +41,7 @@ for epoch in range(epochs):
     if epoch % 5 == 0:
         net.eval()
         test_dataset = CamvidDataSet('test', path)
-        test_loader = data.DataLoader(dataset, batch_size=bsize, num_workers=4, shuffle=True)
+        test_loader = data.DataLoader(test_dataset, batch_size=bsize, num_workers=4, shuffle=True)
         loss = 0
         counter = 0
         for in_batch, target_batch in test_loader:
@@ -49,7 +49,7 @@ for epoch in range(epochs):
                 in_batch, target_batch = in_batch.cuda(), target_batch.cuda()
             out = net(Variable(in_batch))
             counter += 1
-            loss += loss_fn(F.log_softmax(out, 1), Variable(target_batch))
-        loss = loss.data[0] / counter
+            loss += loss_fn(F.log_softmax(out, 1), Variable(target_batch)).data[0]
+        loss = loss / counter
         print(' ========== Testing Loss: {:.5f} =========='.format(loss, epoch))
         net.train()
