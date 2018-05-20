0518e5c253b34e3c6464a8678c0dc40e9ae44e1e
hhsecond <sherinct@live.com>
2018-05-20 10:41:14 +0530

    torchtext example

--- 3.DLWorkFlow/DesignExperiments/torchtext_example.py
+++ 3.DLWorkFlow/DesignExperiments/torchtext_example.py
@@ -1,6 +1,7 @@
 # copied from http://anie.me/On-Torchtext/
 import os
 
+import torch
 import torchtext
 from torchtext import data
 import spacy
@@ -16,6 +17,7 @@ def downloadTREC(path='.'):
     if not os.path.exists('somedummyfile'):
         f = open('somedummyfile', 'w+')
         f.close()
+    # check what's the purpose of this file
     dataset = torchtext.datasets.TREC('somedummyfile', TEXT, LABEL)
     dataset.download(path)
 
@@ -27,10 +29,29 @@ def tokenizer(text):
 # downloadTREC()
 spacy_en = spacy.load('en')
 TEXT = data.Field(sequential=True, tokenize=tokenizer, lower=True)
-LABEL = data.Field(sequential=False, use_vocab=False)
+LABEL = data.Field(sequential=False, use_vocab=True)
 
 
 train, val, test = data.TabularDataset.splits(
-    path='./data/', train='train.tsv',
-    validation='val.tsv', test='test.tsv', format='tsv',
+    path='./data/', train='TRECtrain.tsv',
+    validation='TRECval.tsv', test='TRECtest.tsv', format='tsv',
     fields=[('Text', TEXT), ('Label', LABEL)])
+
+TEXT.build_vocab(train, vectors="glove.6B.50d")
+LABEL.build_vocab(train, vectors="glove.6B.50d")
+train_iter, val_iter, test_iter = data.Iterator.splits(
+    (train, val, test), sort_key=lambda x: len(x.Text),
+    batch_sizes=(32, 99, 99), device=-1)
+
+print(next(iter(test_iter)))
+
+
+class DummyNN(torch.nn.Module):
+
+    def __init__(self, emb_dim):
+        self.embed = torch.nn.Embedding(len(TEXT.vocab), emb_dim)
+        self.embed.weight.data.copy_(TEXT.vocab.vectors)
+
+
+# masked BPTT
+# reversible tokenization
