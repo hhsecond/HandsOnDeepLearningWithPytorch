9420866fcdea3152c2d4fa96a194c3db52f8f80d
hhsecond <sherinct@live.com>
2019-02-21 09:02:20 +0530

    migrated to 1.0

--- 2.ASimpleNeuralNetwork/numpy_like_fizbuz.py
+++ 2.ASimpleNeuralNetwork/numpy_like_fizbuz.py
@@ -1,9 +1,9 @@
 import torch
 
-from datautils import get_data, decoder, check_fizbuz
+from datautils import get_numpy_data, decoder, check_fizbuz
 
 
-epochs = 500
+epochs = 5
 batches = 64
 lr = 0.01
 input_size = 10
@@ -11,7 +11,7 @@ output_size = 4
 hidden_size = 100
 
 
-trX, trY, teX, teY = get_data(input_size)
+trX, trY, teX, teY = get_numpy_data(input_size)
 if torch.cuda.is_available():
     dtype = torch.cuda.FloatTensor
 else:
@@ -21,13 +21,13 @@ x = torch.from_numpy(trX).type(dtype)
 y = torch.from_numpy(trY).type(dtype)
 
 print(x.grad, x.grad_fn, x)
-# None, None, [torch.FloatTensor of size 900x10]
+# None None tensor([[...]])
 
 w1 = torch.randn(input_size, hidden_size, requires_grad=True).type(dtype)
 w2 = torch.randn(hidden_size, output_size, requires_grad=True).type(dtype)
 
 print(w1.grad, w1.grad_fn, w1)
-# None, None, [torch.FloatTensor of size 10x100]
+# None None tensor([[...]])
 
 b1 = torch.zeros(1, hidden_size, requires_grad=True).type(dtype)
 b2 = torch.zeros(1, output_size, requires_grad=True).type(dtype)
@@ -44,7 +44,7 @@ for epoch in range(epochs):
         a2 = a2.add(b1)
 
         print(a2.grad, a2.grad_fn, a2)
-        # None, <AddBackward1 object at 0x7fe4fb786208>, [torch.FloatTensor of size 64x100]
+        # None <AddBackward0 object at 0x7f5f3b9253c8> tensor([[...]])
 
         h2 = a2.sigmoid()
 
@@ -57,28 +57,26 @@ for epoch in range(epochs):
         output.backward()
 
         print(x.grad, x.grad_fn, x)
-        # None, None, [torch.FloatTensor of size 900x10]
+        # None None tensor([[...]])
         print(w1.grad, w1.grad_fn, w1)
-        # [torch.FloatTensor of size 10x100], None, [torch.FloatTensor of size 10x100]
+        # tensor([[...]], None, tensor([[...]]
         print(a2.grad, a2.grad_fn, a2)
-        # None, <AddBackward1 object at 0x7fedcd24f048>, [torch.FloatTensor of size 64x100]
+        # None <AddBackward0 object at 0x7f5f3d42c780> tensor([[...]])
 
-        # Direct manipulation of data outside autograd is not allowed anymore
-        # so this code snippet won't work with pytoch version 0.4+
-        try:
+        # Direct manipulation of data outside autograd is not allowed
+        # when grad flag is True
+        with torch.no_grad():
             w1 -= lr * w1.grad
             w2 -= lr * w2.grad
             b1 -= lr * b1.grad
             b2 -= lr * b2.grad
-            w1.grad.zero_()
-            w2.grad.zero_()
-            b1.grad.zero_()
-            b2.grad.zero_()
-        except RuntimeError as e:
-            raise Exception('Direct manipulation of autograd Variable is not allowed in pytorch \
-version 0.4+. Error thrown by pytorch: {}'.format(e))
+        w1.grad.zero_()
+        w2.grad.zero_()
+        b1.grad.zero_()
+        b2.grad.zero_()
     if epoch % 10:
         print(epoch, output.item())
+breakpoint()
 # traversing the graph using .grad_fn
 print(output.grad_fn)
 # <DivBackward0 object at 0x7eff00ae3ef0>
