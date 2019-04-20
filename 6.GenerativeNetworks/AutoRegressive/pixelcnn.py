37ce329e1fe6553872a08c4c2d11bb57043a598f
hhsecond <sherinct@live.com>
2019-04-20 18:04:50 +0530

    pixel cnn

--- 6.GenerativeNetworks/AutoRegressive/pixelcnn.py
+++ 6.GenerativeNetworks/AutoRegressive/pixelcnn.py
@@ -1,17 +1,13 @@
-import os
-import time
-import sys
-
 import numpy as np
 import torch
 import torch.nn.functional as F
-from torch import nn, optim, cuda, backends
+from torch import nn, optim, backends
 from torch.utils import data
 from torchvision import datasets, transforms, utils
-from flashlight import FlashLight
 backends.cudnn.benchmark = True
 
 CUDA = torch.cuda.is_available()
+
 
 class MaskedConv2d(nn.Conv2d):
     def __init__(self, mask_type, *args, **kwargs):
@@ -27,73 +23,74 @@ class MaskedConv2d(nn.Conv2d):
         self.weight.data *= self.mask
         return super(MaskedConv2d, self).forward(x)
 
+
 fm = 64
 net = nn.Sequential(
-    MaskedConv2d('A', 1,  fm, 7, 1, 3, bias=False), nn.BatchNorm2d(fm), nn.ReLU(True),
-    MaskedConv2d('B', fm, fm, 7, 1, 3, bias=False), nn.BatchNorm2d(fm), nn.ReLU(True),
-    MaskedConv2d('B', fm, fm, 7, 1, 3, bias=False), nn.BatchNorm2d(fm), nn.ReLU(True),
-    MaskedConv2d('B', fm, fm, 7, 1, 3, bias=False), nn.BatchNorm2d(fm), nn.ReLU(True),
-    MaskedConv2d('B', fm, fm, 7, 1, 3, bias=False), nn.BatchNorm2d(fm), nn.ReLU(True),
-    MaskedConv2d('B', fm, fm, 7, 1, 3, bias=False), nn.BatchNorm2d(fm), nn.ReLU(True),
-    MaskedConv2d('B', fm, fm, 7, 1, 3, bias=False), nn.BatchNorm2d(fm), nn.ReLU(True),
-    MaskedConv2d('B', fm, fm, 7, 1, 3, bias=False), nn.BatchNorm2d(fm), nn.ReLU(True),
+    MaskedConv2d('A', 1, fm, 7, 1, 3, bias=False),
+    nn.BatchNorm2d(fm),
+    nn.ReLU(True),
+    MaskedConv2d('B', fm, fm, 7, 1, 3, bias=False),
+    nn.BatchNorm2d(fm),
+    nn.ReLU(True),
+    MaskedConv2d('B', fm, fm, 7, 1, 3, bias=False),
+    nn.BatchNorm2d(fm),
+    nn.ReLU(True),
+    MaskedConv2d('B', fm, fm, 7, 1, 3, bias=False),
+    nn.BatchNorm2d(fm),
+    nn.ReLU(True),
+    MaskedConv2d('B', fm, fm, 7, 1, 3, bias=False),
+    nn.BatchNorm2d(fm),
+    nn.ReLU(True),
+    MaskedConv2d('B', fm, fm, 7, 1, 3, bias=False),
+    nn.BatchNorm2d(fm),
+    nn.ReLU(True),
+    MaskedConv2d('B', fm, fm, 7, 1, 3, bias=False),
+    nn.BatchNorm2d(fm),
+    nn.ReLU(True),
+    MaskedConv2d('B', fm, fm, 7, 1, 3, bias=False),
+    nn.BatchNorm2d(fm),
+    nn.ReLU(True),
     nn.Conv2d(fm, 256, 1))
-print(net)
+
+device = 'cpu'
 if CUDA:
     net.cuda()
+    device = 'cuda'
 
-tr = data.DataLoader(datasets.MNIST('data', train=True, download=True, transform=transforms.ToTensor()),
-                     batch_size=128, shuffle=True, num_workers=1, pin_memory=True)
-te = data.DataLoader(datasets.MNIST('data', train=False, download=True, transform=transforms.ToTensor()),
-                     batch_size=128, shuffle=False, num_workers=1, pin_memory=True)
-if CUDA:
-    sample = torch.Tensor(144, 1, 28, 28).cuda()
-else:
-    sample = torch.Tensor(144, 1, 28, 28)
+train_data = data.DataLoader(
+    datasets.MNIST(
+        'data', train=True, download=True, transform=transforms.ToTensor()),
+    batch_size=128, shuffle=True, num_workers=1)
+test_data = data.DataLoader(
+    datasets.MNIST(
+        'data', train=False, download=True, transform=transforms.ToTensor()),
+    batch_size=128, shuffle=False, num_workers=1)
+sample = torch.Tensor(144, 1, 28, 28).to(device=device)
 optimizer = optim.Adam(net.parameters())
 for epoch in range(25):
-    # train
     err_tr = []
-#     cuda.synchronize()
-    time_tr = time.time()
-    net.train(True)
-    fl = FlashLight(net)
-    for input, _ in tr:
-        if CUDA:
-            input = input.cuda(async=True)
-        target = (input.data[:,0] * 255).long()
+    net.train()
+    for i, (input, _) in enumerate(train_data):
+        input.to(device=device)
+        target = (input.data[:, 0] * 255).long()
         loss = F.cross_entropy(net(input), target)
         print(loss.item())
         err_tr.append(loss.item())
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    # cuda.synchronize()
-    time_tr = time.time() - time_tr
-
-    # compute error on test set
-    err_te = []
-    # cuda.synchronize()
-    time_te = time.time()
-    net.train(False)
-    for input, _ in te:
-        if CUDA:
-            input = input.cuda(async=True)
-        target = (input.data[:,0] * 255).long()
-        loss = F.cross_entropy(net(input), target)
-        err_te.append(loss.data[0])
-    # cuda.synchronize()
-    time_te = time.time() - time_te
-
-    # sample
-    sample.fill_(0)
-    net.train(False)
-    for i in range(28):
-        for j in range(28):
-            out = net(sample, volatile=True)
-            probs = F.softmax(out[:, :, i, j]).data
-            sample[:, :, i, j] = torch.multinomial(probs, 1).float() / 255.
-    utils.save_image(sample, 'sample_{:02d}.png'.format(epoch), nrow=12, padding=0)
-
-    print('epoch={}; nll_tr={:.7f}; nll_te={:.7f}; time_tr={:.1f}s; time_te={:.1f}s'.format(
-        epoch, np.mean(err_tr), np.mean(err_te), time_tr, time_te))
+        if i % 5:
+            # DO validation
+            print('epoch={} - loss={:.7f}'.format(epoch, np.mean(err_tr)))
+            sample.fill_(0)
+            net.eval()
+            with torch.no_grad():
+                for i in range(28):
+                    for j in range(28):
+                        # TODO: put TQDM
+                        out = net(sample)
+                        probs = F.softmax(out[:, :, i, j])
+                        sample[:, :, i, j] = torch.multinomial(
+                            probs, 1).float() / 255.
+            utils.save_image(
+                sample, 'sample_{:02d}.png'.format(epoch), nrow=12, padding=0)
