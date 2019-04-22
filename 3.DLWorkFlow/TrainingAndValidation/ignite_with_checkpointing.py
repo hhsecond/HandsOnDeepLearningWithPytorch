1617b6146e107d2bb319c4fd5cd5995ea676a5fe
hhsecond <sherinct@live.com>
2019-04-22 14:37:00 +0530

    ignite with checkpointing

--- 3.DLWorkFlow/TrainingAndValidation/ignite_with_checkpointing.py
+++ 3.DLWorkFlow/TrainingAndValidation/ignite_with_checkpointing.py
@@ -3,7 +3,6 @@ from __future__ import print_function
 from argparse import ArgumentParser
 import logging
 
-import torch
 from torch import nn
 from torch.optim import SGD
 from torch.utils.data import DataLoader
@@ -11,7 +10,8 @@ import torch.nn.functional as F
 from torchvision.transforms import Compose, ToTensor, Normalize
 from torchvision.datasets import MNIST
 
-from ignite.engine import Events, create_supervised_trainer, create_supervised_evaluator
+from ignite.engine import (
+    Events, create_supervised_trainer, create_supervised_evaluator)
 from ignite.metrics import Accuracy, Loss
 from ignite.handlers import ModelCheckpoint
 
@@ -38,11 +38,15 @@ class Net(nn.Module):
 def get_data_loaders(train_batch_size, val_batch_size):
     data_transform = Compose([ToTensor(), Normalize((0.1307,), (0.3081,))])
 
-    train_loader = DataLoader(MNIST(download=True, root=".", transform=data_transform, train=True),
-                              batch_size=train_batch_size, shuffle=True)
+    train_loader = DataLoader(
+        MNIST(
+            download=True, root=".", transform=data_transform, train=True),
+        batch_size=train_batch_size, shuffle=True)
 
-    val_loader = DataLoader(MNIST(download=False, root=".", transform=data_transform, train=False),
-                            batch_size=val_batch_size, shuffle=False)
+    val_loader = DataLoader(
+        MNIST(
+            download=False, root=".", transform=data_transform, train=False),
+        batch_size=val_batch_size, shuffle=False)
     return train_loader, val_loader
 
 
@@ -50,11 +54,13 @@ def run(train_batch_size, val_batch_size,
         epochs, lr, momentum,
         log_interval, restore_from, crash_iteration=1000):
 
-    train_loader, val_loader = get_data_loaders(train_batch_size, val_batch_size)
+    train_loader, val_loader = get_data_loaders(
+        train_batch_size, val_batch_size)
     model = Net()
     device = 'cpu'
     optimizer = SGD(model.parameters(), lr=lr, momentum=momentum)
-    trainer = create_supervised_trainer(model, optimizer, F.nll_loss, device=device)
+    trainer = create_supervised_trainer(
+        model, optimizer, F.nll_loss, device=device)
     evaluator = create_supervised_evaluator(model,
                                             metrics={'accuracy': Accuracy(),
                                                      'nll': Loss(F.nll_loss)},
@@ -63,7 +69,8 @@ def run(train_batch_size, val_batch_size,
     trainer._logger.setLevel(logging.INFO)
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("%(asctime)s|%(name)s|%(levelname)s| %(message)s")
+    formatter = logging.Formatter(
+        "%(asctime)s|%(name)s|%(levelname)s| %(message)s")
     ch.setFormatter(formatter)
     trainer._logger.addHandler(ch)
 
@@ -72,7 +79,9 @@ def run(train_batch_size, val_batch_size,
         iter = (engine.state.iteration - 1) % len(train_loader) + 1
         if iter % log_interval == 0:
             print("Epoch[{}] Iteration[{}/{}] Loss: {:.2f}"
-                  "".format(engine.state.epoch, iter, len(train_loader), engine.state.output))
+                  "".format(
+                      engine.state.epoch, iter,
+                      len(train_loader), engine.state.output))
 
         if engine.state.iteration == crash_iteration:
             raise Exception("STOP at {}".format(engine.state.iteration))
@@ -83,8 +92,10 @@ def run(train_batch_size, val_batch_size,
         metrics = evaluator.state.metrics
         avg_accuracy = metrics['accuracy']
         avg_nll = metrics['nll']
-        print("Training Results - Epoch: {}  Avg accuracy: {:.2f} Avg loss: {:.2f}"
-              .format(engine.state.epoch, avg_accuracy, avg_nll))
+        print(
+            "Training Results - Epoch: {}\
+            Avg accuracy: {:.2f} Avg loss: {:.2f}".format(
+                engine.state.epoch, avg_accuracy, avg_nll))
 
     @trainer.on(Events.EPOCH_COMPLETED)
     def log_validation_results(engine):
@@ -92,15 +103,19 @@ def run(train_batch_size, val_batch_size,
         metrics = evaluator.state.metrics
         avg_accuracy = metrics['accuracy']
         avg_nll = metrics['nll']
-        print("Validation Results - Epoch: {}  Avg accuracy: {:.2f} Avg loss: {:.2f}"
-              .format(engine.state.epoch, avg_accuracy, avg_nll))
+        print(
+            "Validation Results - Epoch: {}\
+            Avg accuracy: {:.2f} Avg loss: {:.2f}".format(
+                engine.state.epoch, avg_accuracy, avg_nll))
 
     objects_to_checkpoint = {"model": model, "optimizer": optimizer}
-    engine_checkpoint = ModelCheckpoint(dirname="engine_checkpoint",
-                                         filename_prefix='ignite_checking',
-                                         require_empty=False,
-                                         save_interval=100)
-    trainer.add_event_handler(Events.ITERATION_COMPLETED, engine_checkpoint, objects_to_checkpoint)
+    engine_checkpoint = ModelCheckpoint(
+        dirname="engine_checkpoint",
+        filename_prefix='ignite_checking',
+        require_empty=False,
+        save_interval=100)
+    trainer.add_event_handler(
+        Events.ITERATION_COMPLETED, engine_checkpoint, objects_to_checkpoint)
 
     if restore_from == "":
         trainer.run(train_loader, max_epochs=epochs)
@@ -121,8 +136,12 @@ if __name__ == "__main__":
     parser.add_argument('--momentum', type=float, default=0.5,
                         help='SGD momentum (default: 0.5)')
     parser.add_argument('--log_interval', type=int, default=300,
-                        help='how many batches to wait before logging training status')
-    parser.add_argument('--crash_iteration', type=int, default=1000, help='Iteration to suddenly raise as exception')
+                        help='how many batches to wait before logging')
+    parser.add_argument('--restore_from', type=str, default="",
+                        help='location from where the model can be reloaded')
+    parser.add_argument(
+        '--crash_iteration', type=int, default=1000,
+        help='Iteration to suddenly raise as exception')
     args = parser.parse_args()
 
     run(args.batch_size, args.val_batch_size,
